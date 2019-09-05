d31b5293ec4f2b7700fc0492073130aa51081cfb
Karina0709 <53987794+Karina0709@users.noreply.github.com>
2019-09-05 20:47:48 -0300

    Add files via upload

---
+++ 5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+int n1,n2,soma;
+    printf("Valor I\n");
+    scanf("%i",&n1);
+    printf("Valor II\n");
+    scanf("%i",&n2);
+    soma = n1 + n2;
+    printf("%i + %i = %i", n1, n2,soma);
+return 0;
+}
