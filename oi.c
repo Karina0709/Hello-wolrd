2c46971b7d62426c24f13e0a37f523bda5c3a36f
Karina0709 <53987794+Karina0709@users.noreply.github.com>
2019-09-05 20:27:26 -0300

    Add files via upload

---
+++ oi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+    int main()
+    {
+    int n1,n2, multiplicacao;
+        printf ("Inserir Valor 1\n");
+        scanf("%i",&n1);
+        printf ("Inserir Valor 2\n");
+        scanf ("%i",&n2);
+    multiplicacao = n1*n2;
+        printf ("%i x %i = %i", n1,n2,multiplicacao);
+    return 0;
+
+}
